60ace9ee2aca7778ab950961b1014b77e319b75d
Rodo Nore <rodonore@Rodos-iMac.local>
2016-05-10 10:01:13 -0500

    Cuarto modulo del curso

---
+++ exercises/MasFeliz/MasFeliz/MyPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+///variables / constantes
+var nombre = "Rodrigo"
+let edad = 32
+
+//arreglos
+var deportes = ["Soccer"]
+deportes.append("Natacion")
+deportes.append("Golf")
+
+//opcionales
+var apellido : String? = "Noreña"
+apellido = nil
+
+
+
+
+
+
+
+let color = UIColor(red: 30/255, green: 180/255, blue: 20/255, alpha: 1)
+
+arc4random() % 100
---
+++ exercises/MasFeliz/MasFeliz/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MasFeliz
+//
+//  Created by Rodo Nore on 5/10/16.
+//  Copyright © 2016 Rodo Nore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mensajePositivo: UILabel!
+    let colores = Colores()
+    let frases = Datos()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func darUnMensajePositivo() {
+        mensajePositivo.text = frases.regresaFraseFeliz()
+        let colorAleatorio = colores.regresaColorAleatorio()
+        view.backgroundColor = colorAleatorio
+        view.tintColor = colorAleatorio
+    }
+}
+
